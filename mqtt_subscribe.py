fe3e1fbbfd96b6c10a3fe7f8248726b21f27876a
Karley <karley.waguespack@gmail.com>
2023-02-13 14:12:34 -0500

    modified subscribe script to accept command line parameters

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 import paho.mqtt.client as mqtt
+import sys
 
 #subscriber code:
 
+#check command line arguments before doing anything else
+
+if(len(sys.argv) > 1 || len(sys.argv) < 1):
+	print("Wrong number of arguments entered. Correct usage is: python mqtt_subscribe.py [topic]")
+	sys.exit(0)
+
+topic = sys.argv[1]
+
 def on_connect(client, userdata, flags, rc): 
 	print("Connected with result code "+str(rc))
-	client.subscribe("topic/test")
+	client.subscribe(topic)
 
 def on_message(client, userdata, msg): 
-	if msg.payload.decode() == "Hello world!":
-		print("Received payload: Hello world!")
+	result = msg.payload.decode()
+	print("Received payload from topic", topic, ": ", result)
 
 client = mqtt.Client()
 client.connect("192.168.122.232", 1883, 60)
