9f3114d10567a3506293ff50853b2c8ac87ce466
Karley <kwagu14@lsu.edu>
2023-03-26 17:08:35 -0500

    added broadcast alert statement inside of secure boot

--- fingerprinting/mem-forensics.sh
+++ fingerprinting/mem-forensics.sh
@@ -6,17 +6,17 @@ do
 	pid=0
 	threshold = 0.3
 
-	cd /home/karleywa/ids-prototype/fingerprinting/New_Mem_Dumps/$(app)/t1
+	cd ~/ids-prototype/fingerprinting/New_Mem_Dumps/$(app)/t1
 	./../../../memfetch/memfetch $(pid)
-	cd /home/karleywa/ids-prototype/fingerprinting
+	cd ~/ids-prototype/fingerprinting
 	similarity = python3 fingerprinting.py Intra $(app)
 	
 	if [ similarity >= threshold ]
 	then
 		#Broadcast alert
-		
+		python3 ~/ids-prototype/mqtt/mqtt_publish.py "security/alerts" "COMPROMISE"
 		#call secure boot
-		sh /home/karleywa/ids-prototype/secure-boot/secure-boot.sh
+		sh ~/ids-prototype/secure-boot/secure-boot.sh
 	fi
 
 done
