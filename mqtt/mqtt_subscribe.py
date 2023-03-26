998c363798f0b4959c2f702f0245abf8c446dd73
Karley W <kwagu14@lsu.edu>
2023-03-26 16:59:51 -0500

    made changes to subscribe script

--- mqtt/mqtt_subscribe.py
+++ mqtt/mqtt_subscribe.py
@@ -4,6 +4,8 @@ import paho.mqtt.client as mqtt
 import sys
 import os
 
+source = "FROM mqtt_subscribe.py: "
+
 #subscriber code:
 
 #check command line arguments before doing anything else
@@ -15,14 +17,16 @@ if(len(sys.argv) > 2 or len(sys.argv) < 2):
 topic = sys.argv[1]
 
 def on_connect(client, userdata, flags, rc): 
-	print("Connected with result code "+str(rc))
+	print(source + "Connected with result code "+str(rc))
 	client.subscribe(topic)
 
 def on_message(client, userdata, msg): 
 	result = msg.payload.decode()
-	print("Received payload from topic", topic, ": ", result)
-	if topic == "security/alert" and result == "COMPROMISE":
+	print(source + "Received payload from topic", topic, ": ", result)
+	if topic == "security/alerts" and result == "COMPROMISE":
+		print(source + "Beginning boot process")
 		os.system('sh /home/karleywa/ids-prototype/secureboot/secure-boot.sh')
+		print(source + "finished boot process")
 
 client = mqtt.Client()
 client.connect("192.168.0.80", 1883, 60)
