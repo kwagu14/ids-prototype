b497050a46f9f35d398bdc17264529414cd664dd
Karley <kwagu14@lsu.edu>
2023-04-04 17:01:56 -0500

    modified ir-sensor program so that it only stores 100 values instead of 1000

--- application/IR-sensor.py
+++ application/IR-sensor.py
@@ -1,6 +1,6 @@
 # This script is used in the IOT application environment to mimic the capture of sensor data
 
-# It uses an IR sensor; the code captures data from the sensor and stores 1000 data points in 
+# It uses an IR sensor; the code captures data from the sensor and stores 100 data points in 
 # memory at a timee
 
 import RPi.GPIO as GPIO
@@ -11,8 +11,8 @@ GPIO.setup(2, GPIO.IN)
 
 data_list = []
 
-#store the first 500 data values inside of the buffer
-for x in range(0, 1001):
+#store the first 100 data values inside of the buffer
+for x in range(0, 101):
 	data_list.append(GPIO.input(2))
 	sleep(0.1)
 
